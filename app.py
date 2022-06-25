dd79624384ba6d001595b4c10a089e33255733b8
robin-hassan <robiul.hassan12102@gmail.com>
2022-06-24 20:26:10 -0400

    resolve dump json

--- app.py
+++ app.py
@@ -21,11 +21,10 @@ print(f"INSTAGRAM_PASSWORD: {INSTAGRAM_PASSWORD}")
 IMAGE_OUTPUT_FILE = "/app/RGB_IMAGE_OUTPUT.jpg"
 font_path = '/app/Roboto/Roboto-Regular.ttf'
 
-cl = Client()
-
 def convert_image(image_path, quote):
-    im = generate_captioned(quote, image_path=image_path, size=(1080, 1350), font_path=font_path, filter_color=(0, 0, 0, 40))
+    im = generate_captioned(quote, image_path=f"/app/{image_path}", size=(1080, 1350), font_path=font_path, filter_color=(0, 0, 0, 40))
     im.convert('RGB').save(IMAGE_OUTPUT_FILE)
+    print(f"convert image /app/{image_path}")
     # im.show()
 
 def get_image_id():
@@ -34,8 +33,9 @@ def get_image_id():
     [photo] = photos.entries
     print(f"photo_id: {photo.id} and download_link: {photo.link_download}")
     img_data = requests.get(photo.link_download).content
-    with open(photo.id, 'wb') as handler:
+    with open(f"/app/{photo.id}", 'wb') as handler:
         handler.write(img_data)
+    print(f"write image to file /app/{photo.id}")
     return photo.id
 
 def get_quotes():
@@ -44,9 +44,13 @@ def get_quotes():
     return response.json()["content"]
 
 def post_instagram(quote):
-    # cl.load_settings('/app/dump.json')
+    cl = Client()
+    print(f"post to instagram - {quote}")
+    cl.load_settings('/app/dump.json') 
     cl.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
-    cl.account_info().dict()
+    # cl.dump_settings('/app/dump.json')
+    acc_info = cl.account_info().dict()
+    print(f"account info: {acc_info}")
     hash_tags = f"{quote} #bot #love #insta"
     # extract_hashtags(quote)
     media = cl.photo_upload(
@@ -58,7 +62,8 @@ def post_instagram(quote):
             "disable_comments": 0,
         }
     )
-    media.dict()
+    post_info = media.dict()
+    print(f"img post info {post_info}")
 
 def main():
     quote =  get_quotes()
